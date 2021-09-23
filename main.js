3ae16087c637ed40b2641f817a417e533f5d041b
Rodgeno Albury <rodgenoalbury@gmail.com>
2021-09-23 16:16:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,47 @@
 // Add imports here
+const BIP39 = require("bip39");
+const hdkey = require("ethereumjs-wallet/hdkey")
+const Wallet = require('ethereumjs-wallet')
+const keccak256 = require('js-sha3').keccak256;
+const EthereumTx = require('ethereumjs-tx')
+var isValid = BIP39.validateMnemonic("Enter your mnemonic here")
 
 
 
 // Add functions here
+function generateMnemonic(){
+    return BIP39.generateMnemonic()
+}
 
+function generateSeed(mnemonic){
+    return BIP39.mnemonicToSeed(mnemonic)
+}
 
+function generatePrivKey(mnemonic){
+    const seed = generateSeed(mnemonic)
+    return hdkey.fromMasterSeed(seed).derivePath(`m/44'/60'/0'/0/0`).getWallet().getPrivateKey()
+}
 
+function derivePubKey(privKey){
+    const wallet = Wallet.fromPrivateKey(privKey)    
+    return wallet.getPublicKey()
+}
+
+function deriveEthAddress(pubKey){
+    const address = keccak256(pubKey) // keccak256 hash of  publicKey
+    // Get the last 20 bytes of the public key
+    return "0x" + address.substring(address.length - 40, address.length)    
+}
+
+function signTx(privKey, txData){
+    const tx = new EthereumTx(txData)
+    tx.sign(privKey)
+    return tx
+}
+
+function getSignerAddress(signedTx){
+    return "0x" + signedTx.getSenderAddress().toString('hex')
+  }
 /*
 
 Do not edit code below this line.
